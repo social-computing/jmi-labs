7fe6f3c20ae43035a123b6e63d966b304bfd9979
fvs <none@none>
2011-12-14 15:11:13 +0100

    fix + viadeo

---
+++ web-labs/src/main/java/com/socialcomputing/labs/viadeo/ViadeoRestProvider.java
@@ -0,0 +1,238 @@
+package com.socialcomputing.labs.viadeo;
+
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
+
+import com.socialcomputing.wps.server.planDictionnary.connectors.datastore.Attribute;
+import com.socialcomputing.wps.server.planDictionnary.connectors.datastore.Entity;
+import com.socialcomputing.wps.server.planDictionnary.connectors.datastore.StoreHelper;
+import com.socialcomputing.wps.server.planDictionnary.connectors.datastore.social.Person;
+import com.socialcomputing.wps.server.planDictionnary.connectors.datastore.social.SocialHelper;
+import com.socialcomputing.wps.server.planDictionnary.connectors.utils.UrlHelper;
+import com.sun.jersey.api.Responses;
+
+@Path("/viadeo")
+public class ViadeoRestProvider {
+
+    public static final String CLIENT_ID = "SocialComputingMapViTBUC";
+    public static final String CLIENT_SECRET = "bDKYAIKwD1Lys";
+    public static final String APP_URL = "http://labs.just-map-it.com/viadeo/";
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @GET
+    @Path("maps/{kind}.json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String kind(@Context HttpServletRequest request, @PathParam("kind") String kind, @QueryParam("access_token") String token) {
+        HttpSession session = request.getSession(true);
+        String key = kind;
+        String result = ( String)session.getAttribute( key);
+        if (result == null || result.length() == 0) {
+           result = kind(kind, token);
+           session.setAttribute( key, result);
+        }
+        return result;
+    }
+
+    @GET
+    @Path("image-proxy")
+    @Produces("image/*")
+    public Response getThumbnail( @HeaderParam("Accept-Encoding") String encoding, @HeaderParam("If-Modified-Since") String cache, @HeaderParam("If-Modified-Since") String modified, @HeaderParam("If-None-Match") String match, @QueryParam("url") String url) {
+        UrlHelper urlHelper = new UrlHelper( url);
+        try {
+            urlHelper.addHeader( "Accept-Encoding", encoding);
+            urlHelper.addHeader( "If-Modified-Since", modified);
+            urlHelper.addHeader( "If-None-Match", match);
+            urlHelper.addHeader( "Cache-Control", cache);
+            urlHelper.openConnections();
+            HttpURLConnection connection = (HttpURLConnection) urlHelper.getConnection();
+            String tag = connection.getHeaderField( "Etag");
+            if( tag == null) tag = "";
+            if( tag.startsWith("\"")) tag = tag.substring( 1);
+            if( tag.endsWith("\"")) tag = tag.substring( 0, tag.length()-1);
+            return Response.ok( urlHelper.getStream(), urlHelper.getContentType())
+                .lastModified( new Date( connection.getLastModified()))
+                .tag( tag)
+                .status( connection.getResponseCode())
+                .header( "Content-Length", connection.getContentLength())
+                .build();
+        }
+        catch (Exception e) {
+            Response.status( Responses.NOT_FOUND);
+        }
+        return null;
+    }
+
+    private String kind(String kind, String token) {
+        StoreHelper storeHelper = new StoreHelper();
+        try {
+            SocialHelper socialHelper = new SocialHelper( storeHelper);
+            
+            // Liste contacts
+            UrlHelper urlHelper = new UrlHelper();
+            urlHelper.setUrl( "https://api.viadeo.com/me/contacts.json");
+            urlHelper.addParameter( "limit", "50");
+            urlHelper.addParameter( "user_detail", "none");
+            urlHelper.addParameter( "access_token", token);
+            urlHelper.openConnections();
+            JsonNode node = mapper.readTree(urlHelper.getStream());
+            List<String> friendslist = new ArrayList<String>();
+            ArrayNode friends = (ArrayNode)node.get( "data");
+            do {
+                for( JsonNode friend : friends) {
+                    Person person = socialHelper.addPerson( friend.get("id").getTextValue());
+                    person.addProperty("name", friend.get("name").getTextValue());
+                    person.addProperty("url", friend.get("link").getTextValue());
+                    friendslist.add( person.getId());
+                }
+                friends = null;
+                String next = node.get( "paging").get( "next").getTextValue();
+                if( next != "") {
+                    UrlHelper nextHelper = new UrlHelper();
+                    nextHelper.setUrl( next);
+                    nextHelper.openConnections();
+                    node = mapper.readTree( nextHelper.getStream());
+                    friends = (ArrayNode)node.get( "data");
+                }
+            } while (friends != null);  
+            
+            if( kind.equalsIgnoreCase( "contacts")) {
+                // My self
+                UrlHelper uh = new UrlHelper();
+                uh.setUrl( "https://api.viadeo.com/me.json");
+                uh.addParameter("access_token", token);
+                uh.openConnections();
+                JsonNode me = mapper.readTree(uh.getStream());
+                storeHelper.addGlobal( "$MY_VIADEO_ID", me.get("id").getTextValue());
+                
+                // Les amis entre eux 
+                for (String friend : friendslist) {
+                    UrlHelper uh1 = new UrlHelper();
+                    uh1.setUrl( "https://api.viadeo.com/" + friend + "/mutual_contacts.json");
+                    uh1.addParameter( "limit", "50");
+                    uh1.addParameter("access_token", token);
+                    uh1.openConnections();
+                    node = mapper.readTree( uh1.getStream());
+                    friends = (ArrayNode)node.get( "data");
+                    while( friends != null) {
+                        for( JsonNode friendJson : friends) {
+                            socialHelper.setFriendShip( friend, friendJson.get("id").getTextValue());
+                        }
+                        friends = null;
+                        String next = node.get( "paging").get( "next").getTextValue();
+                        if( next != "") {
+                            UrlHelper nextHelper = new UrlHelper();
+                            nextHelper.setUrl( next);
+                            nextHelper.openConnections();
+                            node = mapper.readTree( nextHelper.getStream());
+                            friends = (ArrayNode)node.get( "data");
+                        }
+                    }
+                }
+                
+                // Je suis ami avec tous mes amis
+                //setFriendShip((String)me.get("id"), friendslist);
+
+                // Delete entities with only one attribute 
+                Set<String> toRemove = new HashSet<String>();
+                for( Entity entity : storeHelper.getEntities().values()) {
+                    if( entity.getAttributes().size() == 1) {
+                        toRemove.add( entity.getId());
+                    }
+                }
+                for( String id : toRemove) {
+                    storeHelper.removeEntity( id);
+                }
+            }
+            else {
+                // My self
+                UrlHelper uh = new UrlHelper();
+                uh.setUrl( "https://api.viadeo.com/me.json");
+                uh.addParameter("access_token", token);
+                uh.openConnections();
+                JsonNode me = mapper.readTree(uh.getStream());
+                String myId = me.get("id").getTextValue();
+                Attribute attribute = storeHelper.addAttribute( myId);
+                attribute.addProperty( "name", me.get("name").getTextValue());
+                attribute.addProperty( "url", me.get("link").getTextValue());
+                storeHelper.addGlobal( "$MY_VIADEO_ID", myId);
+                friendslist.add( myId);
+                
+                for( String friend : friendslist) {
+                    UrlHelper urlHelper2 = new UrlHelper();
+                    urlHelper2.setUrl( "https://api.viadeo.com/" + friend + "/" + kind + ".json");
+                    urlHelper2.addParameter( "limit", "50");
+                    urlHelper2.addParameter( "access_token", token);
+                    urlHelper2.openConnections();
+                    
+                    JsonNode node2 = mapper.readTree(urlHelper2.getStream());
+                    ArrayNode kinds = (ArrayNode)node2.get( "data");
+                    if( kinds != null) {
+                        for( JsonNode curkind : kinds) {
+                            if (curkind.get("id") != null && curkind.get("name") != null) {
+                                Entity entity = storeHelper.addEntity( curkind.get("id").getTextValue());
+                                entity.addProperty( "name", curkind.get("name").getTextValue());
+                                entity.addProperty( "url", curkind.get("link").getTextValue());
+                                entity.addAttribute( attribute, 1);
+                            }
+                        }
+                    }
+                }
+                // Delete entities with only one attribute 
+                Set<String> toRemove = new HashSet<String>();
+                for( Entity entity : storeHelper.getEntities().values()) {
+                    if( entity.getAttributes().size() == 1) {
+                        toRemove.add( entity.getId());
+                    }
+                }
+                for( String id : toRemove) {
+                    //removeEntity( id);
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return storeHelper.toJson();
+    }
+
+    public static String GetAccessToken(String code) {
+        //return "260661a8436bd15ca45c24a4b5967dce";
+        String token = "";
+        UrlHelper urlHelper = new UrlHelper( UrlHelper.Type.POST, "https://secure.viadeo.com/oauth-provider/access_token2");
+        urlHelper.addParameter( "grant_type", "authorization_code");
+        urlHelper.addParameter( "client_id", ViadeoRestProvider.CLIENT_ID);
+        urlHelper.addParameter( "client_secret", ViadeoRestProvider.CLIENT_SECRET);
+        urlHelper.addParameter( "redirect_uri", ViadeoRestProvider.APP_URL);
+        urlHelper.addParameter( "code", code);
+        try {
+            urlHelper.openConnections();
+            JsonNode node = mapper.readTree( urlHelper.getStream());
+            token = node.get( "access_token").getTextValue();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return token;
+    }
+}
--- web-labs/src/main/webapp/viadeo/index.jsp
+++ web-labs/src/main/webapp/viadeo/index.jsp
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0014)about:internet -->
+<%@page import="com.socialcomputing.labs.viadeo.ViadeoRestProvider"%>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
  <head>
 <title>Just Map It! Viadeo</title>
@@ -7,14 +7,15 @@
 if( code == null) { %>
 <title>Redirection</title>
 <meta name="robots" content="noindex,follow" />
-<meta http-equiv="refresh" content="0; url=https://secure.viadeo.com/oauth-provider/authorize2?response_type=code&display=popup&lang=en&client_id=SocialComputingMapViTBUC&redirect_uri=http://labs.just-map-it.com/viadeo/index.jsp" />
+<!--meta http-equiv="refresh" content="0; url=https://secure.viadeo.com/oauth-provider/authorize2?response_type=code&display=popup&lang=en&client_id=<%=ViadeoRestProvider.CLIENT_ID%>&redirect_uri=<%=java.net.URLEncoder.encode( ViadeoRestProvider.APP_URL, "UTF-8")%>" /-->
 </head>
 <body>
+<script> top.location.href='https://secure.viadeo.com/oauth-provider/authorize2?response_type=code&display=popup&lang=en&client_id=<%=ViadeoRestProvider.CLIENT_ID%>&redirect_uri=<%=java.net.URLEncoder.encode(ViadeoRestProvider.APP_URL, "UTF-8")%>'</script>
 </body>
 </html>
-<%} else {%>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <style type="text/css" media="screen"> 
+<%} else {
+String authcode = ViadeoRestProvider.GetAccessToken(code);%>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style type="text/css" media="screen">
 html, body {
 	height: 100%;
@@ -36,46 +37,90 @@ object:focus {
 }
 </style>
 <link rel="stylesheet" type="text/css" href="../client/flex/history/history.css" />
+<script type="text/javascript" src="../js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="../client/flex/history/history.js"></script>
 <link rel="stylesheet" href="../css/main.css"/>
 
-<script type="text/javascript">
-function navigate( url, target) {
-	window.open( url, target);
-}
+<script type="text/javascript"> 
+  function empty() {
+	Alert("Sorry, the map is empty");
+  }
+  function error( error) {
+	Alert( "Sorry, an error occured." + error);
+  }
+  function navigate( url) {
+ 	 window.open( url, "_blank");
+  }
+  function focus( args) {
+	var parameters = {};
+	parameters["entityId"] = args[0];
+	parameters["feed"] = args[2];
+	completeParameters( parameters);
+	document.getElementById("jmi-viadeo").compute( parameters);
+  }
+  function center( args) {
+	var parameters = {};
+	parameters["attributeId"] = args[0];
+	parameters["analysisProfile"] = "DiscoveryProfile";
+	completeParameters( parameters);
+	document.getElementById("jmi-viadeo").compute( parameters);
+  }
+  function completeParameters( parameters) {
+	 parameters.allowDomain = "*";
+	 parameters.wpsserverurl = "http://localhost:8080/jmi-server";
+     parameters.viadeoserverurl = "http://localhost:8080/web-labs";
+	 //parameters.wpsserverurl = "http://server.just-map-it.com";
+     //parameters.allocineserverurl = "http://labs.just-map-it.com";
+	 parameters.wpsplanname = "Viadeo";
+	 parameters.authcode = '<%=authcode%>';
+	 parameters.kind = $('#kind option:selected')[0].value;
+	 parameters.jsessionid = '<%=session.getId()%>';
+  }
 </script>
   
 <script type="text/javascript" src="../client/flex/swfobject.js"></script>
 <script type="text/javascript">
+$(document).ready(function() {
    var swfVersionStr = "10.0.0";
    var xiSwfUrlStr = "../client/flex/playerProductInstall.swf";
    var flashvars = {};
-   flashvars.wpsserverurl = "http://server.just-map-it.com/";
-   flashvars.wpsplanname = "Viadeo";
-   flashvars.authcode = '<%=code%>';
+   completeParameters( flashvars);
    flashvars.analysisProfile = "GlobalProfile";
-   flashvars.kind = "contacts";
    var params = {};
    params.quality = "high";
    params.bgcolor = "#FFFFFF";
    params.allowscriptaccess = "sameDomain";
    params.allowfullscreen = "true";
    var attributes = {};
-   attributes.id = "wps-viadeo";
-   attributes.name = "wps-viadeo";
+   attributes.id = "jmi-viadeo";
+   attributes.name = "jmi-viadeo";
    attributes.align = "middle";
    swfobject.embedSWF(
-           "../client/flex/jmi-viadeo-1.0-SNAPSHOT.swf", "flashContent", 
+           "../client/flex/jmi-flex-1.0-SNAPSHOT.swf", "flashContent", 
            "100%", "100%", 
            swfVersionStr, xiSwfUrlStr, 
            flashvars, params, attributes);
-	swfobject.createCSS("#flashContent", "display:block;text-align:left;");
+   
+	  $('#kind').change(function(){
+			 var parameters = {};
+			 completeParameters( parameters);
+			 parameters.analysisProfile = "GlobalProfile";
+			 $('#jmi-viadeo')[0].compute( parameters);
+			});
+});	 
 </script>
 </head>
 <body>
 <table width="100%" border="0">
 	<tr>
 		<td><a title="Just Map It! Viadeo" href=".."><img alt="Just Map It! Labs" src="../images/justmapit_labs.png" /></a></td>
+		<td>
+			<select id="kind">
+			<option value="contacts" >Contacts</option>
+			<option value="groups" >Groups</option>
+			<option value="likes" >Likes</option>
+			</select>
+		</td>
 		<td align="right"><a title="Just Map It! Viadeo" href="./"><img alt="Just Map It! Viadeo" src="../images/justmapit_viadeo.png" /></a></td>
 	</tr>
 </table>
@@ -93,32 +138,9 @@ function navigate( url, target) {
         </div>
 	   	
        	<noscript>
-            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="100%" height="100%" id="wps-flex-demo">
-                <param name="movie" value="../client/flex/jmi-viadeo-1.0-SNAPSHOT.swf" />
-                <param name="quality" value="high" />
-                <param name="bgcolor" value="#FFFFFF" />
-                <param name="allowScriptAccess" value="sameDomain" />
-                <param name="allowFullScreen" value="true" />
-                <!--[if !IE]>-->
-                <object type="application/x-shockwave-flash" data="../client/flex/jmi-viadeo-1.0-SNAPSHOT.swf" width="100%" height="100%">
-                    <param name="quality" value="high" />
-                    <param name="bgcolor" value="#FFFFFF" />
-                    <param name="allowScriptAccess" value="sameDomain" />
-                    <param name="allowFullScreen" value="true" />
-                <!--<![endif]-->
-                <!--[if gte IE 6]>-->
-                	<p> 
-                		Either scripts and active content are not permitted to run or Adobe Flash Player version
-                		10.0.0 or greater is not installed.
-                	</p>
-                <!--<![endif]-->
-                    <a href="http://www.adobe.com/go/getflashplayer">
-                        <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash Player" />
-                    </a>
-                <!--[if !IE]>-->
-                </object>
-                <!--<![endif]-->
-            </object>
+           <a href="http://www.adobe.com/go/getflashplayer">
+               <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash Player" />
+           </a>
 	    </noscript>		
 	    </div>
    </body>
