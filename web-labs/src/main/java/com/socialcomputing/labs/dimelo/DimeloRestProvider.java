eafc407c29b1df526c72db87902f1263b587cc58
fvs <none@none>
2012-04-16 18:00:25 +0200

    Dimelo

--- web-labs/src/main/java/com/socialcomputing/labs/dimelo/DimeloRestProvider.java
+++ web-labs/src/main/java/com/socialcomputing/labs/dimelo/DimeloRestProvider.java
@@ -1,6 +1,8 @@
 package com.socialcomputing.labs.dimelo;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -27,27 +30,207 @@ import com.sun.jersey.api.Responses;
 @Path("/dimelo")
 public class DimeloRestProvider {
 
-    public static final String IDEA_API_URL = "https://open-adisseo.api.ideas.dimelo.com";
-    //public static final String IDEA_API_URL = "https://open-adisseo.api.ideas.dimelo.info:4432";
-    public static final String USER_API_URL = "https://open-adisseo.api.users.dimelo.com";
-    //public static final String USER_API_URL = "https://open-adisseo.api.users.dimelo.info:4433";
-    public static final String ACCESS_TOKEN = "5eba7151e753e9fede13177a3098832f";
     private static final ObjectMapper mapper = new ObjectMapper();
 
     @GET
-    @Path("maps/map.json")
+    @Path("maps/{kind}.json")
     @Produces(MediaType.APPLICATION_JSON)
-    public String kind(@Context HttpServletRequest request, @QueryParam("query") String query) {
+    public String kind(@Context HttpServletRequest request, 
+                       @PathParam("kind") String kind, 
+                       @QueryParam("access-token") String accessToken, 
+                       @QueryParam("www-url") String wwwUrl, 
+                       @QueryParam("user-url") String userUrl, 
+                       @QueryParam("feedback-url") String feedbackUrl, 
+                       @QueryParam("param") String param) {
         HttpSession session = request.getSession(true);
-        String key = query;
+        String key = kind + "/" + param;
         String result = ( String)session.getAttribute( key);
         if (result == null || result.length() == 0) {
-            result = extract(query);
+            if( kind.equalsIgnoreCase("search")) {
+                result = search(accessToken, wwwUrl, userUrl, feedbackUrl, param);
+            }
+            else if( kind.equalsIgnoreCase("user")) {
+                result = user(accessToken, wwwUrl, userUrl, feedbackUrl, param);
+            }
+            else if( kind.equalsIgnoreCase("feedback")) {
+                result = feedback(accessToken, wwwUrl, userUrl, feedbackUrl, param);
+            }
             session.setAttribute( key, result);
         }
         return result;
     }
 
+    @GET
+    @Path("users/{id}.json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String users(@PathParam("id") String id,
+                       @QueryParam("access_token") String accessToken, 
+                       @QueryParam("user_url") String userUrl) {
+        try {
+            UrlHelper url = new UrlHelper( userUrl + "/1.0/users/" + id);
+            url.addParameter( "access_token", accessToken);
+            url.openConnections();
+            return url.getResult();
+        }
+        catch (Exception e) {
+            return StoreHelper.ErrorToJson(e);
+        }
+    }
+
+    @GET
+    @Path("feedbacks/{id}.json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String feedbacks(@PathParam("id") String id,
+                       @QueryParam("access_token") String accessToken, 
+                       @QueryParam("feedback_url") String feedbackUrl) {
+        try {
+            UrlHelper url = new UrlHelper( feedbackUrl + "/1.0/feedbacks/" + id);
+            url.addParameter( "access_token", accessToken);
+            url.openConnections();
+            return url.getResult();
+        }
+        catch (Exception e) {
+            return StoreHelper.ErrorToJson(e);
+        }
+    }
+    
+    // Search within feedbacks
+    private String search(String accessToken, String wwwUrl, String userUrl, String feedbackUrl, String query) {
+        StoreHelper storeHelper = new StoreHelper();
+        try {
+            UrlHelper urlIdeas = new UrlHelper( feedbackUrl + "/1.0/feedbacks");
+            urlIdeas.addParameter( "access_token", accessToken);
+            urlIdeas.addParameter( "limit", "50");
+            urlIdeas.addParameter( "search", query);
+            urlIdeas.openConnections();
+            JsonNode ideas = mapper.readTree(urlIdeas.getStream());
+            for (JsonNode idea : (ArrayNode) ideas) {
+                Attribute att = this.addIdea( storeHelper, idea);
+                this.addContributors(accessToken, feedbackUrl, storeHelper, att, idea);
+            }
+            urlIdeas.closeConnections();
+            this.fillUsersProperties(accessToken, wwwUrl, userUrl, storeHelper);
+        }
+        catch (Exception e) {
+            return StoreHelper.ErrorToJson(e);
+        }
+        return storeHelper.toJson();
+    }
+
+    // Start from an Id
+    private String feedback(String accessToken, String wwwUrl, String userUrl, String feedbackUrl, String feedback) {
+        StoreHelper storeHelper = new StoreHelper();
+        try {
+            UrlHelper urlIdea = new UrlHelper( feedbackUrl + "/1.0/feedbacks/" + feedback);
+            urlIdea.addParameter( "access_token", accessToken);
+            urlIdea.openConnections();
+            JsonNode idea = mapper.readTree(urlIdea.getStream());
+            Attribute att = this.addIdea( storeHelper, idea);
+            this.addContributors(accessToken, feedbackUrl, storeHelper, att, idea);
+            urlIdea.closeConnections();
+
+            Collection<Entity> ents = new ArrayList<Entity>(storeHelper.getEntities().values());
+            for( Entity ent : ents) {
+                UrlHelper urlIdeas = new UrlHelper( feedbackUrl + "/1.0/feedbacks");
+                urlIdeas.addParameter( "access_token", accessToken);
+                urlIdeas.addParameter( "limit", "50");
+                urlIdeas.addParameter( "user_id", ent.getId());
+                urlIdeas.openConnections();
+                JsonNode ideas = mapper.readTree(urlIdeas.getStream());
+                for (JsonNode idea1 : (ArrayNode) ideas) {
+                    att = this.addIdea( storeHelper, idea1);
+                    this.addContributors(accessToken, feedbackUrl, storeHelper, att, idea1);
+                }
+                urlIdeas.closeConnections();
+            }
+            this.fillUsersProperties(accessToken, wwwUrl, userUrl, storeHelper);
+        }
+        catch (Exception e) {
+            return StoreHelper.ErrorToJson(e);
+        }
+        return storeHelper.toJson();
+    }
+    
+    // Start from an Id
+    private String user(String accessToken, String wwwUrl, String userUrl, String feedbackUrl, String userId) {
+        StoreHelper storeHelper = new StoreHelper();
+        try {
+            Entity ent = storeHelper.addEntity( userId);
+            UrlHelper urlIdeas = new UrlHelper( feedbackUrl + "/1.0/feedbacks");
+            urlIdeas.addParameter( "access_token", accessToken);
+            urlIdeas.addParameter( "limit", "150");
+            urlIdeas.addParameter( "user_id", userId);
+            urlIdeas.openConnections();
+            JsonNode ideas = mapper.readTree(urlIdeas.getStream());
+            for (JsonNode idea1 : (ArrayNode) ideas) {
+                Attribute att = this.addIdea( storeHelper, idea1);
+                ent.addAttribute( att, 1);
+                this.addContributors(accessToken, feedbackUrl, storeHelper, att, idea1);
+            }
+            urlIdeas.closeConnections();
+
+            this.fillUsersProperties(accessToken, wwwUrl, userUrl, storeHelper);
+        }
+        catch (Exception e) {
+            return StoreHelper.ErrorToJson(e);
+        }
+        return storeHelper.toJson();
+    }
+
+    private Attribute addIdea(StoreHelper storeHelper, JsonNode idea) throws Exception {
+        Attribute att = storeHelper.addAttribute(String.valueOf(idea.get("id").getIntValue()));
+        att.addProperty("name", idea.get("title").getTextValue());
+        att.addProperty("url", idea.get("permalink").getTextValue());
+        return att;
+    }
+    
+    private void addContributors(String accessToken, String feedbackUrl, StoreHelper storeHelper, Attribute att, JsonNode idea) throws Exception {
+        // Author
+        Entity ent = storeHelper.addEntity(String.valueOf(idea.get("user_id").getIntValue()));
+        ent.addAttribute( att, 1);
+        // Comments
+        UrlHelper urlComments = new UrlHelper( feedbackUrl + "/1.0/feedbacks/" + att.getId() + "/comments");
+        urlComments.addParameter( "access_token", accessToken);
+        urlComments.openConnections();
+        JsonNode commments = mapper.readTree(urlComments.getStream());
+        for (JsonNode comment : (ArrayNode) commments) {
+            if( comment.has("user_id")) {
+                ent = storeHelper.addEntity(String.valueOf(comment.get("user_id").getIntValue()));
+                ent.addAttribute( att, 1);
+            }
+        }
+        urlComments.closeConnections();
+        // Votes
+        UrlHelper urlVotes = new UrlHelper( feedbackUrl + "/1.0/feedbacks/" + att.getId() + "/votes");
+        urlVotes.addParameter( "access_token", accessToken);
+        urlVotes.openConnections();
+        JsonNode votes = mapper.readTree(urlVotes.getStream());
+        for (JsonNode vote : (ArrayNode) votes) {
+            if( vote.has("user_id")) {
+                ent = storeHelper.addEntity(String.valueOf(vote.get("user_id").getIntValue()));
+                ent.addAttribute( att, 1);
+            }
+        }
+        urlVotes.closeConnections();
+    }
+    
+    private void fillUsersProperties(String accessToken, String wwwUrl, String userUrl, StoreHelper storeHelper) throws Exception {
+        UrlHelper urlUsers = new UrlHelper( userUrl + "/1.0/users");
+        urlUsers.addParameter( "access_token", accessToken);
+        urlUsers.addParameter( "limit", "150");
+        for( Entity ent : storeHelper.getEntities().values()) {
+            urlUsers.addParameter( "ids[]", ent.getId());
+        }
+        urlUsers.openConnections();
+        JsonNode users = mapper.readTree(urlUsers.getStream());
+        for (JsonNode user : (ArrayNode) users) {
+            Entity ent = storeHelper.addEntity(String.valueOf(user.get("id").getIntValue()));
+            ent.addProperty("name", user.get("firstname").getTextValue() + " " + user.get("lastname").getTextValue());        
+            ent.addProperty("url", wwwUrl + "/users/" + ent.getId());
+        }
+        urlUsers.closeConnections();
+    }
+    
     @GET
     @Path("image-proxy")
     @Produces("image/*")
@@ -58,7 +241,7 @@ public class DimeloRestProvider {
             urlHelper.addHeader( "If-Modified-Since", modified);
             urlHelper.addHeader( "If-None-Match", match);
             urlHelper.addHeader( "Cache-Control", cache);
-            urlHelper.openConnections();
+            urlHelper.openConnections();    
             HttpURLConnection connection = (HttpURLConnection) urlHelper.getConnection();
             String tag = connection.getHeaderField( "Etag");
             if( tag == null) tag = "";
@@ -77,74 +260,4 @@ public class DimeloRestProvider {
         return null;
     }
 
-    private String extract(String query) {
-        StoreHelper storeHelper = new StoreHelper();
-        try {
-            UrlHelper urlIdeas = new UrlHelper( DimeloRestProvider.IDEA_API_URL + "/1.0/feedbacks");
-            urlIdeas.addParameter( "access_token", DimeloRestProvider.ACCESS_TOKEN);
-            urlIdeas.addParameter( "limit", "50");
-            urlIdeas.addParameter( "search", query);
-            urlIdeas.openConnections();
-            JsonNode ideas = mapper.readTree(urlIdeas.getStream());
-            for (JsonNode idea : (ArrayNode) ideas) {
-                Attribute att = storeHelper.addAttribute(String.valueOf(idea.get("id").getIntValue()));
-                att.addProperty("name", idea.get("title").getTextValue());
-                att.addProperty("url", idea.get("permalink").getTextValue());
-                
-                // Author
-                Entity ent = storeHelper.addEntity(String.valueOf(idea.get("user_id").getIntValue()));
-                ent.addAttribute( att, 1);
-                // Comments
-                UrlHelper urlComments = new UrlHelper( DimeloRestProvider.IDEA_API_URL + "/1.0/feedbacks/" + att.getId() + "/comments");
-                urlComments.addParameter( "access_token", DimeloRestProvider.ACCESS_TOKEN);
-                urlComments.openConnections();
-                JsonNode commments = mapper.readTree(urlComments.getStream());
-                for (JsonNode comment : (ArrayNode) commments) {
-                    if( comment.has("user_id")) {
-                        ent = storeHelper.addEntity(String.valueOf(comment.get("user_id").getIntValue()));
-                        ent.addAttribute( att, 1);
-                    }
-                }
-                urlComments.closeConnections();
-                // Votes
-                UrlHelper urlVotes = new UrlHelper( DimeloRestProvider.IDEA_API_URL + "/1.0/feedbacks/" + att.getId() + "/comments");
-                urlVotes.addParameter( "access_token", DimeloRestProvider.ACCESS_TOKEN);
-                urlVotes.openConnections();
-                JsonNode votes = mapper.readTree(urlVotes.getStream());
-                for (JsonNode vote : (ArrayNode) votes) {
-                    if( vote.has("user_id")) {
-                        ent = storeHelper.addEntity(String.valueOf(vote.get("user_id").getIntValue()));
-                        ent.addAttribute( att, 1);
-                    }
-                }
-                urlVotes.closeConnections();
-            }
-            urlIdeas.closeConnections();
-
-            UrlHelper urlUsers = new UrlHelper( DimeloRestProvider.USER_API_URL + "/1.0/users");
-            urlUsers.addParameter( "access_token", DimeloRestProvider.ACCESS_TOKEN);
-            for( Entity ent : storeHelper.getEntities().values()) {
-                urlUsers.addParameter( "ids[]", ent.getId());
-            }
-            urlUsers.openConnections();
-            JsonNode users = mapper.readTree(urlUsers.getStream());
-            for (JsonNode user : (ArrayNode) users) {
-                Entity ent = storeHelper.addEntity(String.valueOf(user.get("id").getIntValue()));
-                ent.addProperty("name", user.get("firstname").getTextValue() + " " + user.get("lastname").getTextValue());        
-            }
-            urlUsers.closeConnections();
-        }
-        catch (Exception e) {
-            return StoreHelper.ErrorToJson(e);
-        }
-        return storeHelper.toJson();
-    }
-    
-    // Idead from id
-    // /1.0/feedbacks/:id
-    
-    // Search
-    // /1.0/feedbacks
-    // limit
-    // user_id
 }
--- web-labs/src/main/webapp/dimelo/index.jsp
+++ web-labs/src/main/webapp/dimelo/index.jsp
@@ -20,13 +20,14 @@ img {
 </style>
 <%Boolean inverse = request.getParameter("Inverse") != null;
 String query = request.getParameter("query");
-if( query == null) {
-    query = "";
- }%>
+if( query == null) query = "";
+String kind = request.getParameter("kind");
+if( kind == null) kind = "search";%>
 <link rel="stylesheet" type="text/css" href="../jmi-client/css/jmi-client.css" />
+<script type="text/javascript" src="../js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="../jmi-client/jmi-client.js"></script>
 <script type="text/javascript">
-var breadcrumbTitles = { shortTitle: 'Initial query', longTitle: 'Query: <%=query.replace("'", "\\'")%>' };
+var breadcrumbTitles = { };
 function JMIF_breadcrumbTitlesFunc(event) {
 	if( event.type === JMI.Map.event.EMPTY) {
 		return {shortTitle: 'Sorry, the map is empty.', longTitle: 'Sorry, the map is empty.'};
@@ -35,7 +36,7 @@ function JMIF_breadcrumbTitlesFunc(event) {
 		return {shortTitle: 'Sorry, an error occured.', longTitle: 'Sorry, an error occured. Error: ' + event.message};
 	}
 	return breadcrumbTitles;
-}
+};
 function getParams() {
 	return {
 		map: 'Adisseo',
@@ -43,13 +44,50 @@ function getParams() {
     	//dimeloserverurl: 'http://localhost:8080/web-labs',
 		jsessionid: '<%=session.getId()%>',
 		inverted: <%=inverse%>,
-		query: '<%=query.replace("'", "\\'")%>'
+		kind: '<%=kind%>',
+		param: '<%=query.replace("'", "\\'")%>'
     };
 };
 function GoMap() {
 	var parameters = getParams();
-	parameters.analysisProfile='GlobalProfile';
-	if( parameters.query.length > 0) {
+	if( parameters.param.length > 0) {
+		if( parameters.kind === 'search') {
+			parameters.analysisProfile='GlobalProfile';
+			breadcrumbTitles.shortTitle= 'Initial query';
+			breadcrumbTitles.longTitle= 'Query: <%=query.replace("'", "\\'")%>';
+		}
+		else if( parameters.kind === 'feedback') {
+			var data = {};
+			data.feedback_url = 'https://open-adisseo.api.ideas.dimelo.com';
+			data.access_token = '5eba7151e753e9fede13177a3098832f';
+			$.getJSON( parameters.dimeloserverurl + "/rest/dimelo/feedbacks/<%=query%>.json", data, function( feedback) {
+				breadcrumbTitles.shortTitle = parameters.inverted ? "Focus" : "Centered";
+				breadcrumbTitles.longTitle = breadcrumbTitles.shortTitle + " on: " + feedback.title;
+			});
+			if( parameters.inverted) {
+				parameters.entityId = '<%=query%>';
+			}
+			else {
+				parameters.attributeId = '<%=query%>';
+				parameters.analysisProfile = "DiscoveryProfile";
+			}
+		}
+		else if( parameters.kind === 'user') {
+			var data = {};
+			data.user_url = 'https://open-adisseo.api.users.dimelo.com';
+			data.access_token = '5eba7151e753e9fede13177a3098832f';
+			$.getJSON( parameters.dimeloserverurl + "/rest/dimelo/users/<%=query%>.json", data, function( user) {
+				breadcrumbTitles.shortTitle = parameters.inverted ? "Focus" : "Centered";
+				breadcrumbTitles.longTitle = breadcrumbTitles.shortTitle + " on: " + user.firstname + ' ' + user.lastname;
+			});
+			if( parameters.inverted) {
+				parameters.attributeId = '<%=query%>';
+				parameters.analysisProfile = "DiscoveryProfile";
+			}
+			else {
+				parameters.entityId = '<%=query%>';
+			}
+		};
 		var map = JMI.Map({
 					parent: 'map', 
 					clientUrl: '../jmi-client/', 
@@ -71,14 +109,14 @@ function GoMap() {
 };
 function JMIF_Navigate(map, url) {
 	window.open( url, "_blank");
-}
+};
 function JMIF_Focus(map, args) {
 	var parameters = getParams();
 	parameters.entityId = args[0];
 	breadcrumbTitles.shortTitle = "Focus";
 	breadcrumbTitles.longTitle = "Focus on: " + args[1];
 	map.compute( parameters);
-}
+};
 function JMIF_Center(map, args) {
 	var parameters = getParams();
 	parameters.attributeId = args[0];
@@ -86,7 +124,19 @@ function JMIF_Center(map, args) {
 	breadcrumbTitles.shortTitle = "Centered";
 	breadcrumbTitles.longTitle = "Centered on: " + args[1];
 	map.compute( parameters);
-}
+};
+function JMIF_goFeedback(id) {
+	document.getElementById('query').value = id;
+	document.getElementById('kind').value = 'feedback';
+	document.getElementById('main').submit();
+	return false;
+};
+function JMIF_goUser(id) {
+	document.getElementById('query').value = id;
+	document.getElementById('kind').value = 'user';
+	document.getElementById('main').submit();
+	return false;
+};
 </script>
 <jsp:include page="../ga.jsp" />
 </head>
@@ -96,7 +146,12 @@ function JMIF_Center(map, args) {
 	<tr>
 		<td><a title="Just Map It! Labs" href=".."><img alt="Just Map It! Labs" src="../images/justmapit_labs.png" /></a></td>
 		<td>
-			<input type="text" name="query" title="Query" size="80" value="<%=query.replace("\"", "&quot;")%>" />
+			<select id="kind" name="kind">
+				<option value="search" <%=(kind.equalsIgnoreCase("search") ? "selected" : "")%>>Search ideas</option>
+				<option value="feedback" <%=(kind.equalsIgnoreCase("feedback") ? "selected" : "")%>>Idea</option>
+				<option value="user" <%=(kind.equalsIgnoreCase("user") ? "selected" : "")%>>User</option>
+			</select>
+			<input type="text" id="query" name="query" title="Query" size="80" value="<%=query.replace("\"", "&quot;")%>" />
 			<input type="submit" value="Just Map It!" />
 			<input type="checkbox" name="Inverse" <%=inverse ? "checked" : ""%> onclick="document.getElementById('main').submit();"/>Inverse
 		</td>
@@ -105,6 +160,16 @@ function JMIF_Center(map, args) {
 </table>
 </form>
 <div id="breadcrumb">&nbsp;</div>
-<div id="map"></div>
+<div id="map">
+<%if(query.length() == 0) {%>
+    Start from idea <a href="" onclick="return JMIF_goFeedback('89448');">Promote A-dry product through academic use</a><br/>
+    Start from idea <a href="" onclick="return JMIF_goFeedback('81501');">Use emulsion factors to improve fat digestibility in poultry and/or in pigs</a><br/>
+    Start from idea <a href="" onclick="return JMIF_goFeedback('84017');">Rovabio and Rhodimet in Liquid Feeding in Swine</a><br/>
+    <br/><br/>
+    Start from user <a href="" onclick="return JMIF_goUser('166014');">Stefan Bohne</a><br/>
+    Start from user <a href="" onclick="return JMIF_goUser('160328');">Pierre-André Geraert</a><br/>
+    Start from user <a href="" onclick="return JMIF_goUser('165934');">Pierre-André Geraert</a><br/>
+<%}%>
+</div>
 </body>
 </html>
