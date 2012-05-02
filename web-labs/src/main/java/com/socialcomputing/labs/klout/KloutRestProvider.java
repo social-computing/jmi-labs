311c130406b2b453fa77ac9a91aec421c1f54d72
Jonathan Dray <jonthan.dray@gmail.com>
2012-05-02 10:40:33 +0200

    Group API calls : 5 twitter names are allowed per api call

--- web-labs/src/main/java/com/socialcomputing/labs/klout/KloutRestProvider.java
+++ web-labs/src/main/java/com/socialcomputing/labs/klout/KloutRestProvider.java
@@ -1,6 +1,8 @@
 package com.socialcomputing.labs.klout;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,6 +13,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -27,6 +30,7 @@ import com.sun.jersey.api.container.MappableContainerException;
 public class KloutRestProvider {
 
     private static final ObjectMapper mapper = new ObjectMapper();
+    private static final int USERS_PER_REQUEST = 5;
     
     @GET
     @Path("maps/map.json")
@@ -60,7 +64,7 @@ public class KloutRestProvider {
     }
     
     
-    private static void addEntity(StoreHelper storeHelper, MapParameters mapParameters, String userId, int level) 
+    private static void addEntity(StoreHelper storeHelper, MapParameters mapParameters, String query, int level) 
     		throws JsonProcessingException, IOException, JMIException {
     	
     	// lowest level reached, nothing todo
@@ -69,34 +73,72 @@ public class KloutRestProvider {
     	// Query the api 
     	UrlHelper urlNodes = new UrlHelper(mapParameters.getURL());
         urlNodes.addParameter("key", mapParameters.accessToken);
-        urlNodes.addParameter("users", userId);
+        urlNodes.addParameter("users", query);
+        
         try {
-			// TODO : only add the entity if a result is found
-        	// If the user was already processed, skip it
-        	// Else add it in the store
-        	if(storeHelper.getEntity(userId) == null) {
-        		Entity ent = storeHelper.addEntity(userId);
-        		ent.addProperty("name", userId);
-        		
-        		// Then query the api to get the user's influencees
-        		urlNodes.openConnections();
-        		JsonNode result = mapper.readTree(urlNodes.getStream());
-        		if(result.has("users")) {
-        			for (JsonNode user : (ArrayNode) result.get("users")) {
+    		// Find given users and their influencees / influencers
+    		urlNodes.openConnections();
+    		JsonNode result = mapper.readTree(urlNodes.getStream());
+
+    		if(result.has("users")) {
+    			// Add each found user as an entity if it's not already stored 
+    			for (JsonNode user : (ArrayNode) result.get("users")) {
+    				String userId = user.get("twitter_screen_name").get("screen_name").getTextValue();
+    	        	// If the user was already processed, skip it
+    	        	// Else add it in the store
+    	        	if(storeHelper.getEntity(userId) == null) {
+    	        		Entity ent = storeHelper.addEntity(userId);
+    	        		ent.addProperty("name", userId);
+    				
         				if(user.has(mapParameters.mapType)) {
-        					JsonNode influencees = user.get(mapParameters.mapType);
+        					JsonNode influencees = user.get(mapParameters.mapType); 
+        					
+        					List<String> users = new LinkedList<String>();
+        					for (JsonNode influencee : (ArrayNode) influencees) {
+        						String twitterScreenName = influencee.get("twitter_screen_name").getTextValue();
+        						if(storeHelper.getEntity(twitterScreenName) == null) {
+        							Attribute att = storeHelper.addAttribute(twitterScreenName);
+            						att.addProperty("name", twitterScreenName);
+            						ent.addAttribute(att, influencee.get("kscore").getIntValue());
+            						if(storeHelper.getEntity(twitterScreenName) == null) {
+            							// addEntity(storeHelper, mapParameters, twitterScreenName, level - 1);
+            							users.add(twitterScreenName);
+            							if(users.size() == USERS_PER_REQUEST) {
+            								addEntity(storeHelper, mapParameters, StringUtils.join(users, ','), level - 1);
+            								users = new LinkedList<String>();
+            							}
+            						}
+        						}
+        					}
+        					
+        					if(users.size() > 0) {
+        						addEntity(storeHelper, mapParameters, StringUtils.join(users, ','), level - 1);
+        					}
+        					
+        					/*
+        					int i = 0;
+        					int max = users.size();
+        					while(i + USERS_PER_REQUEST <= max) {
+        						
+        					}
+        					*/
+        					
+        					/*
         					for (JsonNode influencee : (ArrayNode) influencees) {
         						String twitterScreenName = influencee.get("twitter_screen_name").getTextValue();
         						Attribute att = storeHelper.addAttribute(twitterScreenName);
         						att.addProperty("name", twitterScreenName);
         						ent.addAttribute(att, influencee.get("kscore").getIntValue());
         						
-        						addEntity(storeHelper, mapParameters, twitterScreenName, level - 1);
-        					}
-        				}            		
-        			}
-        		}
-        	}
+        						// Optimisation : only call if the userid is not already registered as an entity
+        						if(storeHelper.getEntity(twitterScreenName) == null) {
+        							addEntity(storeHelper, mapParameters, twitterScreenName, level - 1);
+        						}
+        					}*/
+        				}
+    	        	}
+    			}
+    		}
 	        
 		}
         finally {
